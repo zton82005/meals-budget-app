364d03e34dd891ffb0482df2c71f5fd71d301b92
zton82005 <zton82005@gmail.com>
2023-09-23 12:25:50 +0800

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import React, { useState } from 'react';
+import { useState } from 'react';
+import Image from "next/image";
 import Budget from "@/components/budget";
 
 export default function Home() {
@@ -28,32 +29,40 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-2 sm:p-24 md:max-w-700">
+    <main className="flex min-h-screen flex-col items-center justify-center p-5 w-full md:max-w-700">
       <div className="flex flex-col">
+        <div className="flex justify-center items-center">
+        <Image className="mb-5" src="/logo.png" width={80} height={80} priority alt="App logo" />
+        </div>
         <h1 className="text-2xl text-center mb-5">Daily Meals Budget Planner</h1>
-        <div className="flex flex-col gap-2">
-          <div className="mb-"> <span className="text-white">Budget amount: </span>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 px-4 items-center">
+      <div className="text-center p-3">
+        <span className="text-white block">Budget amount:</span>
         <input
           type="number"
-          className="text-gray-700 px-2"
+          className="text-gray-700 p-2 w-full md:w-auto mx-auto md:mx-0"
           value={budgetValue === '' ? '' : budgetValue.toString()}
           onChange={handleBudgetChange}
-        /></div>
-          <div><span className="text-white">Number of Days: </span>
+        />
+      </div>
+      <div className="text-center p-3">
+        <span className="text-white block">Number of Days:</span>
         <input
           type="number"
-          className="text-gray-700 px-2"
+          className="text-gray-700 p-2 w-full md:w-auto mx-auto md:mx-0"
           value={daysValue === '' ? '' : daysValue.toString()}
           onChange={handleDaysChange}
-        /></div>
-        </div>
+        />
+      </div>
+</div>
+
        
         
       {typeof budgetValue === 'number' && typeof daysValue === 'number' && (
         <Budget budget={budgetValue} days={daysValue} />
       )}
       </div>
-        <button className="border border-yellow-500 rounded text-yellow-500 py-3 px-5 hover:bg-slate-100 hover:text-black m-10">Find recipes for this budget</button>
+        <button className="border border-yellow-500 rounded-full text-yellow-500 py-3 px-5 hover:bg-slate-100 hover:text-black m-10">Find recipes for this budget</button>
     </main>
   );
 }
--- components/budget.tsx
+++ components/budget.tsx
@@ -123,31 +123,51 @@ export default function Budget({ budget, days }: BudgetProps) {
   }, [selectedOption, dailyBudget]);
 
   return (
-    <div className="flex flex-col mt-4">
-      <p className="mb-2">Your budget for {days} days is ₱{budget}</p>
-      <p className="mb-2">Daily budget is <span className="text-yellow-500">₱{isNaN(dailyBudget) ? '0' : dailyBudget.toFixed(2)}</span></p>
+    <div className="flex flex-col mt-4 w-full">
+      <p className="mb-2 text-center mb-2">Your budget for {days} days is ₱{budget}</p>
+      <div className="border rounded-full border-white text-center mb-4 w-full m-auto p-5">
+        <p className="mb-2 text-center">Daily budget is </p>
+        <span className="text-yellow-500 text-4xl font-bold text-center mb-4">₱{isNaN(dailyBudget) ? '0' : dailyBudget.toFixed(2)}</span>
+      </div>
 
-      <label htmlFor="budgetType">Select your budget distribution:</label>
+      <label htmlFor="budgetType" className="text-center mb-2">Select your budget distribution:</label>
       <select
-        className="text-gray-700 mb-2"
-        id="budgetType"
-        value={selectedOption}
-        onChange={handleOptionChange}
-      >
-        <option value="25-25-25-25 Balance">25-25-25-25 Balance</option>
-        <option value="30-30-30-10 Balance Less Snack">30-30-30-10 Balance Less Snack</option>
-        <option value="15-35-35-15 Lunch & Dinner Focus">15-35-35-15 Lunch & Dinner Focus</option>
-        <option value="20-30-40-10 Dinner Focus">20-30-40-10 Dinner Focus</option>
-        <option value="20-40-30-10 Lunch Focus">20-40-30-10 Lunch Focus</option>
-        <option value="20-40-40-0 Lunch & Dinner Focus w/ No Snack">20-40-40-0 Lunch & Dinner Focus w/ No Snack</option>
-      </select>
-      <p className="mb-2">Percentage Distribution (Breakfast, Lunch, Dinner, Snack)</p>
-      <p className="mb-2">Budget Distribution: {selectedOption}</p>
+  className="text-gray-700 mb-2 w-full md:max-w-md lg:max-w-lg xl:max-w-xl p-2"
+  id="budgetType"
+  value={selectedOption}
+  onChange={handleOptionChange}
+>
+  <option value="25-25-25-25 Balance">25-25-25-25 Balance</option>
+  <option value="30-30-30-10 Balance Less Snack">30-30-30-10 Balance Less Snack</option>
+  <option value="15-35-35-15 Lunch & Dinner Focus">15-35-35-15 Lunch & Dinner Focus</option>
+  <option value="20-30-40-10 Dinner Focus">20-30-40-10 Dinner Focus</option>
+  <option value="20-40-30-10 Lunch Focus">20-40-30-10 Lunch Focus</option>
+  <option value="20-40-40-0 Lunch & Dinner Focus w/ No Snack">20-40-40-0 Lunch & Dinner Focus, No Snack</option>
+</select>
+
+      <p className="text-center mb-2">Percentage Distribution:</p>
+      <p className="mb-4 text-center">
+  <span className="inline-block rounded-full px-4 border border-blue-500 px-2 py-1 mr-2 text-blue-500 hover:bg-white  transition duration-500 ease-in-out">
+    Breakfast
+  </span>
+  <span className="inline-block rounded-full px-4 border border-green-500 px-2 py-1 mr-2 text-green-500 hover:bg-white  transition duration-500 ease-in-out">
+    Lunch
+  </span>
+  <span className="inline-block rounded-full px-4 border border-red-500 px-2 py-1 mr-2 text-red-500 hover:bg-white  transition duration-500 ease-in-out">
+    Dinner
+  </span>
+  <span className="inline-block rounded-full px-4 border border-purple-500 px-2 py-1 text-purple-500 hover:bg-white  transition duration-500 ease-in-out">
+    Snack
+  </span>
+</p>
+
+
+      <p className="mb-2 text-center">Budget Distribution: {selectedOption}</p>
       
-      <p>Breakfast: <span className="text-yellow-500">₱{isNaN(breakfast) ? '0' : breakfast.toFixed(2)}</span></p>
-      <p>Lunch: <span className="text-yellow-500">₱{isNaN(lunch) ? '0' : lunch.toFixed(2)}</span></p>
-      <p>Dinner: <span className="text-yellow-500">₱{isNaN(dinner) ? '0' : dinner.toFixed(2)}</span></p>
-      <p>Snack: <span className="text-yellow-500">₱{isNaN(snack) ? '0' : snack.toFixed(2)}</span></p>
+      <p className="text-center">Breakfast: <span className="text-yellow-500">₱{isNaN(breakfast) ? '0' : breakfast.toFixed(2)}</span></p>
+      <p className="text-center">Lunch: <span className="text-yellow-500">₱{isNaN(lunch) ? '0' : lunch.toFixed(2)}</span></p>
+      <p className="text-center">Dinner: <span className="text-yellow-500">₱{isNaN(dinner) ? '0' : dinner.toFixed(2)}</span></p>
+      <p className="text-center">Snack: <span className="text-yellow-500">₱{isNaN(snack) ? '0' : snack.toFixed(2)}</span></p>
     </div>
   );
 }
